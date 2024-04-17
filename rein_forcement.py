f60745e02123fd1a71bee54bfa9c18029a218a72
Takeru Otsu <takeru.o.2002@gmail.com>
2024-04-17 13:39:04 +0900

    Update

--- rein_forcement.py
+++ rein_forcement.py
@@ -4,15 +4,13 @@ from IPython.display import Image as IImage
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-from gymnasium.wrappers import RecordVideo 
 
 class reinforcement_learning:
     def __init__(self):
         self.epsilon = 0.5
         self.alpha = 0.5
         self.gamma = 0.99
-        self.max_episodes = 300
-        # self.max_steps = env.spec.max_episode_steps
+        self.max_episodes = 2000
         self.successed_episode = 0
     
     def save_gif(self, rgb_arrays, filename, duration=60):
@@ -65,11 +63,14 @@ class reinforcement_learning:
         alpha = 0.1
         q_table[state, action] = q_table[state, action] + alpha * (r + gamma * q_table[next_state, next_action] - q_table[state, action])
     
-    def plot(self, rewards, algorithm):
+    def plot(self, rewards, rewards_mean, rewards_median, algorithm):
         
-        plt.plot(np.arange(self.max_episodes), rewards)
+        plt.plot(np.arange(self.max_episodes), rewards, label="episode_reward")
+        plt.plot(np.arange(self.max_episodes), rewards_mean, label="episode_reward_mean")
+        plt.plot(np.arange(self.max_episodes), rewards_median, label="episode_reward_median")
         plt.xlabel("episode")
         plt.ylabel("reward")
+        plt.legend()
         plt.title(algorithm)
         plt.savefig("./" + algorithm + "/" + algorithm + ".png")
         plt.show()
@@ -82,25 +83,24 @@ class reinforcement_learning:
     def run_Q_learing(self, task):
         env = gym.make(task, render_mode="rgb_array")
         q_table = np.random.uniform(0,1,(255,env.action_space.n))
-        max_steps = 200
-        imgs = []
+        max_steps = env.spec.max_episode_steps
         rewards = []
+        rewards_mean = []
+        rewards_median = []
         successed_episodes = 0
+        
         for episode in range(self.max_episodes):
             observation = env.reset()
-            episode_filename = "./q-learning/q-learning" + str(episode + 1) + ".gif"
 
-            state = self.digitize_state(observation[0], env)
-            
-            imgs = [env.render()]
-            
+            state = self.digitize_state(observation[0], env)\
+
             episode_reward = 0
             for step in range(max_steps):
                 
                 # epsilon-greedy法により行動を決定
                 action = self.decide_action(episode, state, q_table, env)
                 # 次の状態を獲得
-                next_observation, reward, terminated, _, _ = env.step(action)
+                next_observation, reward, terminated, truncated, _ = env.step(action)
                 
                 # 報酬獲得
                 if terminated:
@@ -113,6 +113,7 @@ class reinforcement_learning:
                     reward = 1
 
                 episode_reward += reward
+
                 # 状態を離散値に変換
                 next_state = self.digitize_state(next_observation, env)
 
@@ -120,35 +121,28 @@ class reinforcement_learning:
                 self.q_learning(q_table, state, action, next_state, reward)
                 state = next_state
                 
-                if episode == self.max_episodes-1:
-
-                    # env = gym.wrappers.RecordVideo(env, video_folder="./", episode_trigger=True, disable_logger=True)
-
-                    imgs.append(env.render())
-
-                if terminated or step == max_steps-1:
+                if terminated or truncated or step == max_steps-1:
                     env.reset()
                     rewards.append(episode_reward)
+                    rewards_mean.append(np.mean(rewards))
+                    rewards_median.append(np.median(rewards))
                     break
         
-        self.plot(rewards, "q-learning")
-        self.render(imgs, episode_filename)
+        self.plot(rewards, rewards_mean, rewards_median, "q-learning")
+        env.close()
     
     def run_SARSA(self, task):
         env = gym.make(task, render_mode="rgb_array")
         q_table = np.random.uniform(0,1,(255,env.action_space.n))
-        max_steps = 200
-        imgs = []
         rewards = []
+        rewards_mean = []
+        rewards_median = []
         successed_episodes = 0
+        max_steps = env.spec.max_episode_steps
         for episode in range(self.max_episodes):
             observation = env.reset()
 
-            episode_filename = "./sarsa/sarsa" + str(episode + 1) + ".gif"
-
             state = self.digitize_state(observation[0], env)
-            
-            imgs = [env.render()]
             
             episode_reward = 0
 
@@ -157,15 +151,13 @@ class reinforcement_learning:
             for step in range(max_steps):
 
                 # 次の状態を獲得
-                next_observation, reward, terminated, _, _ = env.step(action)
+                next_observation, reward, terminated, truncated, _ = env.step(action)
                 
                 # 報酬獲得
                 if terminated:
-                    # print("terminated {} episode in {} step".format(episode+1, step+1))
                     if step < max_steps-5:
                         reward = -100
                     else:
-                        # print("{} episode successed".format(episode + 1))
                         successed_episodes += 1
                         reward = 1
                 else:
@@ -182,19 +174,15 @@ class reinforcement_learning:
                 self.sarsa(q_table, state, action, next_state, next_action, reward)
                 state = next_state
                 action = next_action
-                
-                if episode == self.max_episodes-1:
-                    
-                    imgs.append(env.render())
-                    # screen = env.render()
 
-                if terminated or step == max_steps-1:
+                if terminated or truncated or step == max_steps-1:
                     rewards.append(episode_reward)
+                    rewards_mean.append(np.mean(rewards))
+                    rewards_median.append(np.median(rewards))
                     env.reset()
-                    # print("episode{} finished ofter {} timesteps, total reward {}".format(episode, step+1, total_reward))
                     break
-        self.plot(rewards, "sarsa")
-        self.render(imgs, episode_filename)
+        self.plot(rewards, rewards_mean, rewards_median, "sarsa")
+        env.close()
 
 if __name__ == "__main__":
     task = "CartPole-v1"
